c1c56b64fbd67e222ebec35878fee6d491656027
Eldos <eldos.baktybekov@gmail.com>
2024-04-21 23:39:12 +0600

    :sparkles: Add production productivity API call

--- src/connections/dodo_is_api.py
+++ src/connections/dodo_is_api.py
@@ -108,3 +108,61 @@ class DodoIsApiConnection:
             params=request_query_params,
             headers=headers,
         )
+
+    async def get_production_productivity(
+            self,
+            *,
+            access_token: SecretStr,
+            from_datetime: datetime,
+            to_datetime: datetime,
+            unit_uuids: Iterable[UUID],
+    ) -> httpx.Response:
+        """
+        Retrieve performance metrics for the selected period
+        and specified pizzerias (units).
+
+        Keyword Args:
+            unit_uuids: Up to 30 unit UUIDs can be listed in one request.
+            from_datetime: The date range start.
+            to_datetime: The date range end. It must not exceed 31 days.
+            access_token: Access token.
+
+        How the "Speed" Metric is Calculated:
+
+        Speed is the percentage of products prepared faster than
+        the established standard time.
+        To calculate speed, the number of products prepared on time for a period
+        is divided by the total number of products for that period.
+        ⚠️ Daily speed is not calculated as the average speed for shifts.
+
+        Shift 1: Prepared 10 products on time out of 100 total.
+        Speed for the shift: 10 / 100 = 10%.
+
+        Shift 2: Prepared 50 products on time out of 150 total.
+        Speed for the shift: 50 / 150 = 30%.
+
+        The speed metric will be calculated as (10 + 50) / (100 + 150) = 24.
+        If you calculate the average per shift,
+        the result will be incorrect: (10 + 30) / 2 = 20.
+
+        References:
+            Documentation: https://docs.dodois.io/docs/dodo-is/0693bf4a07b8e-proizvodstvo-proizvoditelnost
+
+        Scopes:
+            - productionefficiency - production efficiency.
+
+        Returns:
+            httpx.Response.
+        """
+        request_query_params = build_request_query_params(
+            from_datetime=from_datetime,
+            to_datetime=to_datetime,
+            unit_uuids=unit_uuids,
+        )
+        url = '/production/productivity'
+        headers = build_headers(access_token=access_token)
+        return await self.__http_client.get(
+            url=url,
+            params=request_query_params,
+            headers=headers,
+        )
