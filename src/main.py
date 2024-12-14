0b4527118f895788c38dd0833fd1a45da34a52a8
Eldos <eldos.baktybekov@gmail.com>
2024-12-14 13:15:44 +0600

    :loud_sound: Setup logging

--- src/main.py
+++ src/main.py
@@ -2,9 +2,15 @@ from faststream import FastStream
 from faststream.rabbit import RabbitBroker
 
 from handlers import router
+from src.logger import setup_logging
 
 
-broker = RabbitBroker('amqp://localhost:5672')
+broker = RabbitBroker("amqp://localhost:5672")
 app = FastStream(broker)
 
 broker.include_router(router)
+
+
+@app.on_startup
+async def on_startup() -> None:
+    setup_logging()
