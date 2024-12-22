f2967d460a69cd325fc2bed01374d86cee77bd28
Eldos <eldos.baktybekov@gmail.com>
2024-12-22 15:24:31 +0600

    :label: Add AccountTokensAndUnitUUIDs model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,10 @@
-from .auth_credentials import AccountTokens
+from .auth_credentials import AccountTokens, AccountTokensAndUnitUUIDs
 from .dodo_is_api import (
     LateDeliveryVoucher,
     UnitProductivityStatistics,
     UnitSales,
 )
+from .fetch_results import FetchResult
 from .events import Event, SpecificChatsEvent
 from .reports import UnitLateDeliveryVouchers
 from .units import Unit
@@ -11,8 +12,10 @@ from .units import Unit
 
 __all__ = (
     "AccountTokens",
+    "AccountTokensAndUnitUUIDs",
     "LateDeliveryVoucher",
     "UnitProductivityStatistics",
+    "FetchResult",
     "Event",
     "SpecificChatsEvent",
     "UnitLateDeliveryVouchers",
